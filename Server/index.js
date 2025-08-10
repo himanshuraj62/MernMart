fd1f63b67b08966ca67d049982bf47acb81f5a9e
Himanshu rajpoot <himanshurajpoot2233@gmail.com>
2025-08-10 13:57:51 +0530

    updating the package-lock.json

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,9 @@ import uploadRouter from './routes/upload.router.js'
 import subCategoryRouter from './routes/subCategory.route.js'
 import productRouter from './routes/product.route.js'
 import cartRouter from './routes/cart.route.js'
+import addressRouter from './routes/address.route.js'
+import orderRouter from './routes/order.route.js'
+// import orderRouter from './routes/order.route.js'
 
 const app = express ();
 app.use(cors({
@@ -40,7 +43,8 @@ app.use("/api/file",uploadRouter)
 app.use("/api/subcategory",subCategoryRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/product",productRouter)
-
+app.use("/api/address",addressRouter)
+app.use("/api/order" ,orderRouter)
 
 connectDB().then(()=>{
     app.listen(PORT , ()=>{
